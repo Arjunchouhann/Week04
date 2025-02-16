8fcb19a1e02d19754ad5a920cee86ff367eaf4bc
Arjunchouhann <chouhanarjun05112003@gmail.com>
2025-02-16 23:54:56 +0530

    [Arjun] Add.exercise 4

---
+++ Day-06/src/main/java/com/annotations/exercise/exercise4/TaskManager.java
@@ -0,0 +1,9 @@
+package com.annotations.exercise.execise4;
+
+public class TaskManager {
+
+    @TaskInfo(priority = 1, assignedTo = "Rononoa Zoro")
+    public void performTask() {
+        System.out.println("Performing task...");
+    }
+}
