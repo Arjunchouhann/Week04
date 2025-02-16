dd4aef208ca93be886040f58c5df00494043161b
Arjunchouhann <chouhanarjun05112003@gmail.com>
2025-02-16 23:55:06 +0530

    [Arjun] Add.exercise 1

---
+++ Day-06/src/main/java/com/annotations/exercise/exercise1/AnimalMain.java
@@ -0,0 +1,8 @@
+package com.annotations.exercise.exercise1;
+public class AnimalMain {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.makeSound();
+    }
+}
