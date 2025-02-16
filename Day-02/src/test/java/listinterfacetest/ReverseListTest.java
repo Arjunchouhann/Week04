ff803f81b33b12c673eccb1cad2563743413aa6c
Arjunchouhann <chouhanarjun05112003@gmail.com>
2025-02-17 00:51:38 +0530

    [Arjun] Add. testing of collections

---
+++ Day-02/src/test/java/listinterfacetest/ReverseListTest.java
@@ -0,0 +1,18 @@
+package listinterfacetest;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+import listinterface.reversealist.*;
+import org.junit.jupiter.api.Test;
+
+public class ReverseListTest {
+    //reverse array list test
+    @Test
+    void reverseArrayListTest(){
+
+    }
+    //reverse linkedlist test
+    @Test
+    void reverseLinkedListTest(){
+
+    }
+}
