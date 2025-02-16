3480e353494fefd91c1048f48b88b5635179e78c
Arjunchouhann <chouhanarjun05112003@gmail.com>
2025-02-17 01:49:45 +0530

    [Arjun] Add.testing of exceptions

---
+++ Day-04/src/test/java/CheckedExceptionTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+import checkedexception.*;
+public class CheckedExceptionTest {
+    @Test
+    void textCheckedException(){
+        CheckedException ch = new CheckedException();
+
+    }
+}
