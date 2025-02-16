853fdcfec56f6e218b1da056a52215c46bc18fef
Arjunchouhann <chouhanarjun05112003@gmail.com>
2025-02-16 23:53:56 +0530

    [Arjun] Add.exercise 5

---
+++ Day-06/src/main/java/com/annotations/exercise/exercise5/BugReport.java
@@ -0,0 +1,10 @@
+package com.annotations.exercise.execise5;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Repeatable(BugReports.class)
+public @interface BugReport {
+    String description();
+}
