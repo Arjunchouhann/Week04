4d87b5674743272e10a29a7f6eceb8cecbb806c6
Arjunchouhann <chouhanarjun05112003@gmail.com>
2025-02-16 23:48:20 +0530

    [Arjun] Add.json field

---
+++ Day-06/src/main/java/com/annotations/advancedproblem/jsonfield/JsonFieldMain.java
@@ -0,0 +1,10 @@
+package com.annotations.advancedproblem.jsonfield;
+
+public class JsonFieldMain {
+
+    public static void main(String[] args) {
+        User user = new User("Ankit Patel", 25);
+        String json = JsonSerializer.toJson(user);
+        System.out.println(json);
+    }
+}
